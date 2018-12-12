4ce6dae088ed17210ff83feeefdcca8b88227117
Dola-Ng <ngjingling99@gmail.com>
2018-12-12 23:00:00 +0800

    final

--- helloworld/views.py
+++ helloworld/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate
 from django.contrib import auth
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.views.gengeric.edit import CreateView, UpdateView, DeleteView
 from django import template
 from booklist.models import Booklist
 import datetime
@@ -10,7 +11,10 @@ import datetime
 #from django.contrib.auth.forms import UserCreationForm
 #from login_1.models import register
 
-
+class BookCreate(CreateView):
+	model = booklist
+	fields = ['book','review','date']
+	success_url = 
 def index(request):
 	loop = [0,1,2,3,4,5,6]
 	booklist = Booklist.objects.all()
@@ -24,6 +28,7 @@ def index(request):
 	return render (request, 'html.html',{'booklist':booklist,'loop':loop,'book':book,'review':review,'date':date})
 
 
+
 def inyo(request):
 	booklist = Booklist.objects.all()
 	book = request.POST.get('book',False)
@@ -35,17 +40,3 @@ def inyo(request):
 
 	return render (request, 'homepage.html',{'booklist':booklist,'book':book,'review':review,'date':date})
 
-def update(request):
-	reviews = Booklist.objects.all()
-	template = 'homepage.html'
-	if request.method =='GET':
-		inyo = Booklist.objects.create(reviews=reviews)
-		return render(request,template,{'inyo':inyo})
-
-	inyo = inyo(request.POST,reviews=reviews)
-	if not inyo.is_valid():
-		return render(request, template,{'inyo':inyo})
-
-	inyo.save()
-	message.success(request,'Successfully Update!')
-	return redirect('/homepage/')
