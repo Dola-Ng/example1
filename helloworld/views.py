afa735876a644c4d2e45a6d20775c1c94e780948
Dola-Ng <ngjingling99@gmail.com>
2019-01-03 14:04:10 +0800

    hii

--- helloworld/views.py
+++ helloworld/views.py
@@ -5,12 +5,14 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.views.gengeric.edit import CreateView, UpdateView, DeleteView
 from django import template
+
 import datetime
 
 #from django.contrib.auth.forms import UserCreationForm
 #from login_1.models import register
 
-def 
+def index(request):
+
 
 	return render (request, 'index.html')
 
